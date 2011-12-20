9c06e6d22619b252dd8cb3b22bcdc04adc4160da
Vladimir Minakov <vladimir.minakov.83@gmail.com>
2011-12-20 18:20:43 +0300

    asd

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -2,7 +2,7 @@
 var run = function(application) {
     if (navigator.userAgent.indexOf('Browzr') > -1) {
         // blackberry
-        setTimeout(application, 250)	
+        setTimeout(application, 250);
     } else {
         // attach to deviceready event, which is fired when phonegap is all good to go.
         x$(document).on('deviceready', application, false);
@@ -14,7 +14,7 @@ var run = function(application) {
 
 // shows id passed
 , display = function(id) {
-    x$(["#welcome", "#map", "#settings"]).each(function(e, i) {
+    x$(["#welcome", "#map", "#settings", "game"]).each(function(e, i) {
         var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
         x$(e).css({ 'display':display })
     });
